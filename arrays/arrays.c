1fedcd30a5aa75d8d99a1c5d96344108a45f08df
theDrake <dcdrake42@gmail.com>
2015-10-13 16:51:47 -0600

    Update arrays.c

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -13,35 +13,26 @@ Description: Contains five functions that use and/or modify arrays, including
 
 Description: Reverses the order of elements in an array.
 
-     Inputs: a    – An array of integers (to be reversed).
+     Inputs: a    - An array of integers (to be reversed).
              size - An integer corresponding to the number of elements in "a".
 
     Outputs: None (but array "a" is modified).
 ******************************************************************************/
-void reverse_array(int a[], int size)
-{
-  int index1 = 0;        /* Array index variable, pointing at first element. */
-  int index2 = size - 1; /* Array index variable, pointing at last element.  */
-  int value1;            /* Temporarily holds an array value to be swapped.  */
-  int value2;            /* Temporarily holds an array value to be swapped.  */
+void reverse_array(int a[], int size) {
+  int index1 = 0,
+      index2 = size - 1,
+      value1,
+      value2;
 
-    /* If the size is 0 or 1 (or negative), no reversing will occur.         */
-  if (size < 2)
-  {
+  if (size < 2) {
     return;
   }
-    /*
-     * For arrays of size 2 and above, values are reversed by starting at both
-     * ends and moving toward the center.
-     */
-  while (index1 < index2)
-  {
-    value1    = a[index1];
-    value2    = a[index2];
-    a[index1] = value2;
-    a[index2] = value1;
-    index1++;
-    index2--;
+
+  while (index1 < index2) {
+    value1 = a[index1];
+    value2 = a[index2];
+    a[index1++] = value2;
+    a[index2--] = value1;
   }
 }
 
@@ -51,7 +42,7 @@ void reverse_array(int a[], int size)
 Description: Adds the elements of two arrays and puts the sum of each pair of
              elements into a third array. All arrays should be of equal size.
 
-     Inputs: a    – An array of integers (not to be modified).
+     Inputs: a    - An array of integers (not to be modified).
              b    - An array of integers (not to be modified).
              c    - An array of integers (to contain the sums of elements from
                     "a" and "b").
@@ -60,17 +51,11 @@ Description: Adds the elements of two arrays and puts the sum of each pair of
 
     Outputs: None (but array "c" is modified).
 ******************************************************************************/
-void add_arrays(const int a[], const int b[], int c[], int size)
-{
-  int x;  /* for loop counter */
+void add_arrays(const int a[], const int b[], int c[], int size) {
+  int i;
 
-    /*
-     * Each element in array "c" will be the sum of the elements of arrays "a"
-     * and "b" at the same index value.
-     */
-  for (x = 0; x < size; x++)
-  {
-    c[x] = a[x] + b[x];
+  for (i = 0; i < size; i++) {
+    c[i] = a[i] + b[i];
   }
 }
 
@@ -79,19 +64,17 @@ void add_arrays(const int a[], const int b[], int c[], int size)
 
 Description: Multiplies each element of an array by a given value.
 
-     Inputs: a          – The array of integers to be modified.
+     Inputs: a          - The array of integers to be modified.
              size       - The number of elements in "a".
              multiplier - The integer to be multiplied to each element in "a".
 
     Outputs: None (but array "a" is modified).
 ******************************************************************************/
-void scalar_multiply(int a[], int size, int multiplier)
-{
-  int x;  /* for loop counter */
+void scalar_multiply(int a[], int size, int multiplier) {
+  int i;
 
-  for (x = 0; x < size; x++)
-  {
-    a[x] *= multiplier;
+  for (i = 0; i < size; i++) {
+    a[i] *= multiplier;
   }
 }
 
@@ -103,21 +86,19 @@ Description: Returns the scalar (integer) dot product of two vectors (arrays of
              the product of each pair of elements:
                 (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]) + ...
 
-     Inputs: a    – An array of integers (not to be modified).
-             b    – An array of integers (not to be modified).
+     Inputs: a    - An array of integers (not to be modified).
+             b    - An array of integers (not to be modified).
              size - An integer corresponding to the number of elements in each
                     array.
 
     Outputs: The scalar (integer) dot product of the two arrays.
 ******************************************************************************/
-int dot_product(const int a[], const int b[], int size)
-{
-  int x;                  /* for loop counter                                */
-  int sumOfProducts = 0;  /* To hold the dot product value to be returned.   */
+int dot_product(const int a[], const int b[], int size) {
+  int i;
+  int sumOfProducts = 0;
 
-  for (x = 0; x < size; x++)
-  {
-    sumOfProducts += a[x] * b[x];
+  for (i = 0; i < size; i++) {
+    sumOfProducts += a[i] * b[i];
   }
 
   return sumOfProducts;
@@ -129,15 +110,14 @@ int dot_product(const int a[], const int b[], int size)
 Description: Takes two vectors (arrays of integers), calculates their cross
              product, and stores the cross product in another vector (array).
 
-     Inputs: a – An array of integers (not to be modified).
-             b – An array of integers (not to be modified).
+     Inputs: a - An array of integers (not to be modified).
+             b - An array of integers (not to be modified).
              c - An array of integers (to contain the cross product of "a" and
                  "b").
 
     Outputs: None (but array "c" is modified).
 ******************************************************************************/
-void cross_product(const int a[], const int b[], int c[])
-{
+void cross_product(const int a[], const int b[], int c[]) {
   c[0] = a[1] * b[2] - a[2] * b[1];
   c[1] = a[2] * b[0] - a[0] * b[2];
   c[2] = a[0] * b[1] - a[1] * b[0];

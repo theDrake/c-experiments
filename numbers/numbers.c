4f41920936ccc7bd2be8e1d8118f2f1d68b7959c
theDrake <david@davidcdrake.com>
2015-10-19 21:32:42 -0600

    Update numbers.c

--- numbers/numbers.c
+++ numbers/numbers.c
@@ -10,33 +10,32 @@ Description: Prompts the user for three numbers between 0 and 9.999, then
 #include <math.h>  /* sin, cos, tan, atan */
 #include <stdio.h> /* scanf, printf       */
 
-int main (void)
-{
+int main(void) {
   float input1, input2, input3;
 
-  printf ("Enter 3 numbers between 0 and 9.999: ");
-  scanf ("%f%f%f", &input1, &input2, &input3);
+  printf("Enter 3 numbers between 0 and 9.999: ");
+  scanf("%f%f%f", &input1, &input2, &input3);
 
-  printf ("\nNumber      sin      cos      tan      atan\n"
-          "-------------------------------------------\n");
-  printf ("%.5f %8.3f %8.3f %8.3f %8.3f\n",
-          input1,
-          sin(input1),
-          cos(input1),
-          tan(input1),
-          atan(input1));
-  printf ("%.5f %8.3f %8.3f %8.3f %8.3f\n",
-          input2,
-          sin(input2),
-          cos(input2),
-          tan(input2),
-          atan(input2));
-  printf ("%.5f %8.3f %8.3f %8.3f %8.3f\n",
-          input3,
-          sin(input3),
-          cos(input3),
-          tan(input3),
-          atan(input3));
+  printf("\nNumber      sin      cos      tan      atan\n"
+         "-------------------------------------------\n");
+  printf("%.5f %8.3f %8.3f %8.3f %8.3f\n",
+         input1,
+         sin(input1),
+         cos(input1),
+         tan(input1),
+         atan(input1));
+  printf("%.5f %8.3f %8.3f %8.3f %8.3f\n",
+         input2,
+         sin(input2),
+         cos(input2),
+         tan(input2),
+         atan(input2));
+  printf("%.5f %8.3f %8.3f %8.3f %8.3f\n",
+         input3,
+         sin(input3),
+         cos(input3),
+         tan(input3),
+         atan(input3));
 
   return 0;
 }
